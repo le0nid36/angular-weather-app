389845fd4e6ad82f350fa26b455a5cef04f85cfd
le0nid36 <davitkovskileonid@gmail.com>
2025-09-30 20:52:53 +0200

    first commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(FormsModule, HttpClientModule)
+  ]
+};
